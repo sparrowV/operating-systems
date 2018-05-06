49aefdfe2bd25ed39b6c993285a461caa4020499
Otar Dvalishvili <odval14@freeuni.edu.ge>
2018-05-06 18:35:08 +0000

    add read,filesize syscalls

--- Project2_User-Programs/src/userprog/syscall.c
+++ Project2_User-Programs/src/userprog/syscall.c
@@ -21,6 +21,7 @@ static void open(struct file_desc * open_files);
 static void exit(int code);
 static int get_file_desc(struct file_desc * file_descs);
 bool is_valid_fd(int fd);
+static int read(int fd,uint8_t  * buffer, size_t size);
 
 
 
@@ -111,11 +112,42 @@ syscall_handler (struct intr_frame *f UNUSED)
     }
 
     case SYS_FILESIZE: {
+        validate_uaddr(args+1);
+		lock_acquire(get_file_system_lock());
+
+    int fd = args[1];
+    if(!is_valid_fd(fd)){
+          	lock_release(get_file_system_lock());
+            exit(-1);
+    }
+    struct file *cur_file = thread_current()->file_descs[fd].open_file;
+		f->eax = file_length (cur_file);
+  	lock_release(get_file_system_lock());
 
       break;
     }
 
     case SYS_READ: {
+      validate_uaddr(args+1);
+      validate_uaddr(args+2);
+      validate_uaddr(args+3);
+
+
+      int fd = args[1];
+      uint8_t *buffer = args[2];
+      size_t size = args[3];
+      validate_uaddr(buffer);
+
+      lock_acquire(get_file_system_lock());
+      int res = read(fd,buffer,size);
+      if (res == -1) {
+        lock_release(get_file_system_lock());
+        exit(-1);
+      }
+      f->eax = res;
+      lock_release(get_file_system_lock());
+
+
 
       break;
     }
@@ -131,7 +163,10 @@ syscall_handler (struct intr_frame *f UNUSED)
 
       lock_acquire(get_file_system_lock());
       int res = write(fd,buffer,size);
-      if (res == -1) exit(-1);
+      if (res == -1) {
+          lock_release(get_file_system_lock());
+        exit(-1);
+      }
       f->eax = res;
       lock_release(get_file_system_lock());
       break;
@@ -165,9 +200,34 @@ syscall_handler (struct intr_frame *f UNUSED)
   }
 }
 
+static int read(int fd,uint8_t  * buffer, size_t size) {
+  if (!is_valid_fd(fd) || fd == 1) return -1;
+  if (fd == 0){
+    
+    int index = 0;
+    
+    for(;index < size; index++){
+     	buffer[index] = input_getc(); 
+    }
+   
+    return size;
+  } else {
+    struct file *cur_file = thread_current()->file_descs[fd].open_file;
+ 
+      int s =  file_read(cur_file,buffer,size);
+      //printf("size read is %d\n\n\n\n\n",s);
+      //printf("parameter size read is %d\n\n\n\n\n",size);
+        // printf("buffer is %s\n\n\n",(char *)buffer);
+         return s;
+    
+    
+  }
+
+}
+
 
 static int write(int fd,void * buffer, size_t size) {
-  if (!is_valid_fd(fd)) return -1;
+  if (!is_valid_fd(fd) || fd == 0) return -1;
   if (fd == 1){
      putbuf(buffer,size);
     return size;
@@ -222,5 +282,5 @@ static int get_file_desc(struct file_desc * file_descs) {
 }
 
 bool is_valid_fd(int fd) {
-  return (fd > 0 && fd <= MAX_OPEN_FILES);
+  return (fd >= 0 && fd <= MAX_OPEN_FILES);
 }
